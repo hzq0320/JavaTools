0f15190b3b6566dc61f8ad73c03a35032f295f43
xuerhu <1975798751@qq.com>
2018-07-25 18:46:46 +0800

    commit

---
+++ src/main/java/JavaTools/JavaTools/App.java
@@ -0,0 +1,13 @@
+package JavaTools.JavaTools;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
